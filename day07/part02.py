8f2bd4be60460288e889c6346c43581464f47620
Luisetex <luisetex@gmail.com>
2021-12-07 18:49:29 +0100

    add more detailed explanation for arth. prog.

--- day07/part02.py
+++ day07/part02.py
@@ -6,7 +6,16 @@ with open(INPUT_PATH) as input_file:
 
 possible_positions = range(max(crab_positions)+1)
 
-# each step costs one more fuel: arithmetic progression
+"""
+The cost of the fuel increases by one in each step -> Arithmetic progression
+Sum of members in a finite arithmetic progression: Sn = (n/2) * (a1+an)
+Notation:
+    a1: first element (1)
+    an: last element ( abs(crab_position-possible_position) )
+    d: difference between two consecutive elements a(n-1) and a(n)
+    n: number of "steps" to get from a1 to an. As d = 1, n = an
+For more info: https://en.wikipedia.org/wiki/Arithmetic_progression
+"""
 
 costs = list(map(
     lambda possible_position: sum(
